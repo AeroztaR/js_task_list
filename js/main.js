e7c08134f94e1036764668ecfda5834d1ea32a6a
AeroztaR <jorguinhos89@gmail.com>
2019-01-09 05:05:31 +0500

    add filter tasks feature

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ function loadEventListeners() {
 	taskList.addEventListener('click', removeTask);
 
 	clearBtn.addEventListener('click', clearTasks);
+
+	filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -62,4 +64,20 @@ function clearTasks() {
 	while(taskList.firstChild) {
 		taskList.removeChild(taskList.firstChild);
 	}
+}
+
+// filter tasks
+function filterTasks(e) {
+	const text = e.target.value.toLowerCase();
+
+	document.querySelectorAll('.collection-item').forEach(
+		function(task) {
+			const item = task.firstChild.textContent;
+			if(item.toLowerCase().indexOf(text) != -1) {
+				task.style.display = 'block';
+			} else {
+				task.style.display = 'none';
+			}
+		}
+	);
 }
